087ab076daa5f1495e54f00bf95c91ccab5c762d
liuyangyangwork <liuyangyangwork@163.com>
2017-02-18 17:02:11 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,16 @@ var users=require('../user.json');
             app.post('/login',function(req,res){
                 var name=req.body.name;
                 var user=users[name];
-                console.log(name)
-                console.log(user)
-                console.log(req.body.role)
                 if(user && user.pwd_s==req.body.pwd && req.body.role == "1"){
                     res.cookie('name_s',name);
                     res.cookie('role',req.body.role);
                     res.redirect('/shop');//跳转商家页面
                 }else if(user&&user.pwd_u==req.body.pwd&& req.body.role=="2"){
-                    console.log("用户登录");
                     res.cookie('name_u',name);
                     res.cookie('role',req.body.role);
                     res.redirect('/list');//跳转到用户页面
                 }else{
-                     console.log("test3")
-                    res.sendStatus(404);
+                    res.send("账号密码不匹配或者未选择正确角色，详细请联系管理管.....");
                 }
             });
         }
