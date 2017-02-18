44dfa32edbcb5caa26b58f364e12e44c0690f64f
liuyangyangwork <liuyangyangwork@163.com>
2017-02-18 15:41:43 +0800

    change

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ var app=express();
     server=http.createServer(app);
 //路径与使用view模板为html
 app.set('views',path.join(__dirname,'views'));
+console.log(path.join(__dirname,'views'))
 app.set('view engine','html');
 app.engine('.html',require('ejs').__express);
 //中间用到的插件
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend:false}));
 app.use(cookieParser());
 //设置本地静态资源路径
 app.use(express.static(path.join(__dirname,'public')));
 console.log(path.join(__dirname,'public'))
-require('./routes')(app);  
+require('./routes/index.js')(app);  
 require('./lib/socket')(server);
 server.listen(8085,function(){
     console.log('App start,port 8085.');
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,32 @@
 var users=require('../user.json');
-module.exports=function(app){
-    console.log()
     module.exports=function(app){
             app.get('/',function(req,res){
-                // res.send("hello")
-                // res.render('login');
-                console.log("welcome")
-                res.send('hello world');
+                res.render('login');
             });
             app.get('/list',function(req,res){
                 res.render('list')
             });
-            app.get('/shops',function(req,res){
-                res.render('shops');
+            app.get('/shop',function(req,res){
+                res.render('shop');
             });
             app.post('/login',function(req,res){
                 var name=req.body.name;
                 var user=users[name];
-                if(user && user.pwd_s==req.body.pwd && "1" == req.body.role){
-                    console.log("商家登陆");
+                console.log(name)
+                console.log(user)
+                console.log(req.body.role)
+                if(user && user.pwd_s==req.body.pwd && req.body.role == "1"){
                     res.cookie('name_s',name);
                     res.cookie('role',req.body.role);
-                    res.redirect('/shops');//跳转商家页面
-                }else if(user&&user.pwd_u==req.body.pwd&&"2"==req.body.role){
+                    res.redirect('/shop');//跳转商家页面
+                }else if(user&&user.pwd_u==req.body.pwd&& req.body.role=="2"){
                     console.log("用户登录");
                     res.cookie('name_u',name);
                     res.cookie('role',req.body.role);
                     res.redirect('/list');//跳转到用户页面
                 }else{
+                     console.log("test3")
                     res.sendStatus(404);
                 }
             });
         }
-};
