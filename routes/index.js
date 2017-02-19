5ef4c5195e3412c35800642566675e7ed6257873
liuyangyangwork <liuyangyangwork@163.com>
2017-02-19 14:42:57 +0800

    xiugai

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,24 @@ var users=require('../user.json');
             app.get('/shop',function(req,res){
                 res.render('shop');
             });
-            app.post('/login',function(req,res){
-                var name=req.body.name;
-                var user=users[name];
-                if(user && user.pwd_s==req.body.pwd && req.body.role == "1"){
+            app.post('/login', function(req,res){
+                var name = req.body.name ;
+                var user = users[name];
+                if(user && user.pwd_s == req.body.pwd && "1" == req.body.role){
+                    console.log('商家登录~');
                     res.cookie('name_s',name);
                     res.cookie('role',req.body.role);
-                    res.cookie('service',user.service);
-                    res.redirect('/shop');//跳转商家页面
-                }else if(user&&user.pwd_u==req.body.pwd&& req.body.role=="2"){
+                    res.cookie('service',users[name].service);
+                    res.redirect('/shop'); //转到商家页面
+                }else if(user && user.pwd_u == req.body.pwd && "2" == req.body.role){
+                    console.log('用户登录~');
                     res.cookie('name_u',name);
                     res.cookie('role',req.body.role);
-                    res.cookie('tel',user.tel);
-                    res.redirect('/list');//跳转到用户页面
-                }else{
-                    res.send("账号密码不匹配或者未选择正确角色，详细请联系管理管.....");
+                    res.cookie('tel',users[name].tel);
+                    res.redirect('/list'); //转到用户页面
+                }
+                else{
+                    res.send(404);
                 }
             });
         }
